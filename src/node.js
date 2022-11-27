e162e878f31d729e69f047ed4291e6fa1dc88e4c
= <pangus@duck.com>
2022-11-27 13:53:34 +0100

    fg

---
+++ src/node.js
@@ -0,0 +1,5 @@
+function portalowiec() {
+    console.log("YouTube: https://www.youtube.com");
+    console.log("   Kraina Pango: https://www.youtube.com/@KPango")
+    console.log("   Panguś: https://www.youtube.com/@PangusYT")
+}
